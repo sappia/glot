a5a8daf76f64e110182b8a3faa19cd127e397170
sappia <shreya.appia@gmail.com>
2024-04-29 16:48:22 +0100

    Restructure files

--- app/routers/translate.py
+++ app/routers/translate.py
@@ -1,9 +1,9 @@
 import logging
 import translators as ts
 
-from fastapi import APIRouter
-from schemas.translate import TranslatePayload, TranslateLLMPayload
-from services.translate_llm import translate_llama3
+from fastapi import APIRouter, HTTPException
+from app.schemas.translate import TranslatePayload, TranslateLLMPayload
+from app.services.translate_llm import translate_llama3
 
 router = APIRouter()
 
@@ -18,7 +18,7 @@ def translate(data: TranslatePayload):
         output = ts.translate_text(input_text, from_language=from_lang, to_language=to_lang, translator=translator)
     except Exception as e:
         logging.error(f"Failed to translate: {e}")
-        output = f"Failed to translate: {e}"
+        raise HTTPException(status_code=422, detail=f"Failed to translate")
 
     return {"output": output}
 
@@ -33,6 +33,6 @@ def translate_llm(data: TranslateLLMPayload):
         output = translate_llama3(from_lang, to_lang, input_text)
     except Exception as e:
         logging.error(f"Failed to translate: {e}")
-        output = f"Failed to translate: {e}"
+        raise HTTPException(status_code=422, detail=f"Failed to translate")
 
     return {"output": output}
