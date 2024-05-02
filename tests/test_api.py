3ec405b7d5125bcf0be6c693a17656a494017936
sappia <shreya.appia@gmail.com>
2024-05-02 18:02:17 +0100

    Add a test for summarize_llm endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -49,3 +49,17 @@ def test_translate_llm():
     assert response.json() == {
         "output": "Il fait soleil aujourd'hui."
     }
+
+
+def test_summarize_llm():
+    response = client.post(
+        "/summarize_llm",
+        headers={"Content-Type": "application/json"},
+        json={
+            "input_text": "Generative artificial intelligence (generative AI, GenAI, or GAI) is artificial intelligence capable of generating text, images, videos, or other data using generative models, often in response to prompts. Generative AI models learn the patterns and structure of their input training data and then generate new data that has similar characteristics. Improvements in transformer-based deep neural networks, particularly large language models (LLMs), enabled an AI boom of generative AI systems in the early 2020s. These include chatbots such as ChatGPT, Copilot, Gemini and LLaMA, text-to-image artificial intelligence image generation systems such as Stable Diffusion, Midjourney and DALL-E, and text-to-video AI generators such as Sora. Companies such as OpenAI, Anthropic, Microsoft, Google, and Baidu as well as numerous smaller firms have developed generative AI models. Generative AI has uses across a wide range of industries, including software development, healthcare, finance, entertainment, customer service, sales and marketing, art, writing, fashion, and product design. However, concerns have been raised about the potential misuse of generative AI such as cybercrime, the use of fake news or deepfakes to deceive or manipulate people, and the mass replacement of human jobs."
+            },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "output": "Here is a concise summary:\n\nGenerative artificial intelligence (GAI) can create text, images, videos, or data using patterns learned from training data. GAI models have improved significantly with transformer-based deep neural networks, leading to the development of chatbots, image generators, and video generators by companies like OpenAI, Microsoft, and Google. While GAI has many applications across industries, concerns exist about its potential misuse in areas such as cybercrime, fake news, and job replacement."
+    }
